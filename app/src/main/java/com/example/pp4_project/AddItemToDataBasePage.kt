cfe367c6642ce246387c7457866bf6b7ef42c3bd
jeanmichel3212@gmail.com <jeanmichel3212@gmail.com>
2022-09-08 11:46:19 -0400

    added new empty activity addPage

---
+++ app/src/main/java/com/example/pp4_project/AddItemToDataBasePage.kt
@@ -0,0 +1,11 @@
+package com.example.pp4_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddItemToDataBasePage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_item_to_data_base_page)
+    }
+}
