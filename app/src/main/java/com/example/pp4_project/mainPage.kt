e0379c93dd8ffbf894191414c91e81389646db87
jeanmichel3212@gmail.com <jeanmichel3212@gmail.com>
2022-09-08 11:34:02 -0400

    Implemented UI for mainPage

---
+++ app/src/main/java/com/example/pp4_project/mainPage.kt
@@ -0,0 +1,11 @@
+package com.example.pp4_project
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class mainPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
